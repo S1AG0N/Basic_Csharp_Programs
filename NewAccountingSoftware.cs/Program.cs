81e4063082608029ac94aa72e1d131b9a642649b
phillip ziwada <phillipziwadah@gmail.com>
2025-12-31 12:13:30 +0000

    Initial commit

---
+++ NewAccountingSoftware.cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Accounting
+{
+    static void Main()
+    {
+        Console.WriteLine("Welcome to Acme Accounting Systems n");
+        Console.WriteLine("Remember, we're \"accounting\" on you!");
+        Console.ReadLine();
+
+    }
+}
---
+++ myConsoleProject.cs/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+Console.WriteLine("What is your name?");
+string name = Console.ReadLine();
+Console.WriteLine("Hello, " + name + "!");
+Console.ReadLine();
