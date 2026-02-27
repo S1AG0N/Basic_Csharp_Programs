c3bd08fdc1a212cc4206eef96c4a17f6bebdcb9b
phillip ziwada <phillipziwadah@gmail.com>
2026-02-27 23:57:54 +0000

    added more comments to lines of code

--- ArrayAssignment/Program.cs
+++ ArrayAssignment/Program.cs
@@ -1,25 +1,58 @@
 ﻿using System;
+using System.Collections.Generic; // Required for List<T>
 
-//Pulling the names from a string array
+// -----------------------------
+// SECTION 1: Pulling a name from a string array
+// -----------------------------
 
-string[] names = new string[] { "Prisca", "Phillip", "Patricia", "Steven", "Gift", "Patience", "Perpetua", "Tayana", "Noku", "Talia" };
+// Create a string array containing 10 names
+string[] names = new string[]
+{
+    "Prisca", "Phillip", "Patricia", "Steven",
+    "Gift", "Patience", "Perpetua", "Tayana",
+    "Noku", "Talia"
+};
+
+// Ask the user to enter a number between 1 and 10
 Console.WriteLine("There are 10 names so use any random number between 1-10 to choose a name: ");
+
+// Read user input and convert it to an integer
 int number = int.Parse(Console.ReadLine());
+
+// Check if the number is within valid range
 if (number >= 1 && number <= 10)
 {
+    // Arrays are zero-indexed, so subtract 1 from user input
     Console.WriteLine("The random name generated is: " + names[number - 1]);
 }
 else
 {
-    Console.WriteLine("the number you entered is outside the range");
+    // Display error message if input is outside the allowed range
+    Console.WriteLine("The number you entered is outside the range");
 }
 
-    //Pulling a number from an array
-    int[] numbers = new int[] { 742, 315, 908, 561, 284, 673, 490, 857, 126, 934 };
+
+// -----------------------------
+// SECTION 2: Pulling a number from an integer array
+// -----------------------------
+
+// Create an integer array with 10 three-digit numbers
+int[] numbers = new int[]
+{
+    742, 315, 908, 561, 284,
+    673, 490, 857, 126, 934
+};
+
+// Ask the user to select a position
 Console.WriteLine("Please enter any random number from 1-10:");
+
+// Read and convert input
 int position = int.Parse(Console.ReadLine());
+
+// Validate the position entered
 if (position >= 1 && position <= 10)
 {
+    // Subtract 1 because arrays start at index 0
     Console.WriteLine("Your lucky number is: " + numbers[position - 1]);
 }
 else
@@ -27,8 +60,14 @@ else
     Console.WriteLine("The number you have entered is out of range.");
 }
 
-//List of strings
-List<string> list = new List<string>() {
+
+// -----------------------------
+// SECTION 3: Using a List of strings
+// -----------------------------
+
+// Create a List<string> containing the same names
+List<string> list = new List<string>()
+{
     "Prisca",
     "Phillip",
     "Patricia",
@@ -38,7 +77,14 @@ List<string> list = new List<string>() {
     "Perpetua",
     "Tayana",
     "Noku",
-    "Talia" };
+    "Talia"
+};
+
+// Prompt user to enter a number
 Console.WriteLine("Please enter the number to see the magic name:");
+
+// Convert input to integer
 int num = int.Parse(Console.ReadLine());
+
+// Access the list element (Lists are also zero-indexed)
 Console.WriteLine("Tadaaaa: " + list[num - 1]);
