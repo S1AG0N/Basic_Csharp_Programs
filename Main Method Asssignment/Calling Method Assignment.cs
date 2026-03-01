1ab087d4e0395397357ca8d7d948a755649a68f5
phillip ziwada <phillipziwadah@gmail.com>
2026-03-01 16:53:51 +0000

    first commit

---
+++ Main Method Asssignment/Calling Method Assignment.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling_Method_Asssignment
+{
+    public class Calling_Method_Assignment
+    {
+        // Method 1: Squares the number
+        public int Square(int number)
+        {
+            return number * number;
+        }
+
+        // Method 2: Doubles the number
+        public int Double(int number)
+        {
+            return number * 2;
+        }
+
+        // Method 3: Subtracts 10 from the number
+        public int SubtractTen(int number)
+        {
+            return number - 10;
+        }
+    }
+}
---
+++ Main Method Asssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Calling_Method_Asssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Create an instance of the Calling_Method_Assignment class to access its methods
+            Calling_Method_Assignment math = new Calling_Method_Assignment();
+
+            // Prompt the user to enter a number
+            Console.WriteLine("Enter a number: ");
+
+            // Read the user's input and convert it from a string to an integer
+            int userNum = Convert.ToInt32(Console.ReadLine());
+
+            // Call the Square method and display the result
+            Console.WriteLine("Square: " + math.Square(userNum));
+
+            // Call the Double method and display the result
+            Console.WriteLine("Double: " + math.Double(userNum));
+
+            // Call the SubtractTen method and display the result
+            Console.WriteLine("Subtract 10: " + math.SubtractTen(userNum));
+        }
+    }
+}
